5b3b601b3329cc2c196342f85a2e836c59b70475
Giannoval <giannoval76@gmail.com>
2026-01-04 17:07:41 +0700

    deploy

--- api/src/server.js
+++ api/src/server.js
@@ -1,19 +1,10 @@
+import 'dotenv/config'
 import express from 'express'
-import path from 'path'
-import { ENV } from './config/env.js'
 
 const app = express()
-const __dirname = path.resolve()
 
-app.get("/api/health", (req, res) => {
-    res.status(200).json({ message: "Success" })
+app.get('/api/health', (req, res) => {
+    res.json({ message: 'Success' })
 })
 
-if (ENV.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "../admin/dist")))
-    app.get("/{*any}", (req, res) => {
-        res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"))
-    })
-}
-
-app.listen(ENV.PORT, () => console.log('Server is Running'))
+export default app
